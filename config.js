02608d7e254aee1f67e265eb1c7192ec104f6135
kinjembom123 <86716272+kinjembom123@users.noreply.github.com>
2021-06-30 17:26:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introDesc: `Chơi game không bạn ơi ?`,
     btnIntro: 'hihi',
     title: 'Phải chăng bạn muốn chơi game với tôi ?',
-    desc: '',
+    desc: 'Bạn có muốn chơi game với tôi ?',
     btnYes: 'Chơi đi sợ éo gì',
     btnNo: 'Không rảnh:3',
     question:'tại sao trên tới giới này có 7 tỷ ng mà bạn muốn chơi game với tôi ?',
