f9756ec9b43b6872d0d84b14c83b4f2bd98bca9e
kinjembom123 <86716272+kinjembom123@users.noreply.github.com>
2021-06-30 17:17:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bạn à!',
+    introDesc: `Chơi game không bạn ơi ?`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Phải chăng bạn muốn chơi game với tôi ?',
+    desc: 'Phải chăng bạn cũng muốn chạy bo với tôi ',
+    btnYes: 'Chơi đi sợ éo gì',
+    btnNo: 'Không rảnh:3',
+    question:'tại sao trên tới giới này có 7 tỷ ng mà bạn muốn chơi game với tôi ?',
+    btnReply: 'Gửi cho Quốc',
+    reply: 'Chơi game đi',
+    mess: 'Tôi biết mà bạn muốn chơi game với tôi :))',
+    messDesc: 'Tối nay 7h bắn PUBG đê',
+    btnAccept: 'Okiiiii lun ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
